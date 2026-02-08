d6b93e02c25458d1e09a1295b34127b7a7df9cd5
defnvary <defnvary@gmail.com>
2026-02-08 19:43:02 +0530

    querymodel_search file has some problem (not from my side)

---
+++ databases/relational_model_4.py
@@ -0,0 +1,7 @@
+# more than one table or relation between multiple tables
+#
+from PySide6.QtSql import QSqlRelation, QSqlRelationalTableModel
+
+relation = QSqlRelation()
+
+## This one has to wait
